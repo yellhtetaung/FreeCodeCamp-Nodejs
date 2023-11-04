47c9d77566880ba776ac7eb9853c7807225e9784
yellhtetaung <yehtetaung.dev@gmail.com>
2023-11-04 14:55:39 +0630

    init

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,46 +1,45 @@
 const express = require("express");
 const app = express();
-let { people } = require("./data");
+let {people} = require("./data");
 
 // static assets
 app.use(express.static("./methods-public"));
 // parse form data
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 // parse json data
 app.use(express.json());
 
 app.get("/api/people", (req, res) => {
-	res.status(200).json({ success: true, data: people });
+  res.status(200).json({success: true, data: people});
 });
 
 app.post("/api/people", (req, res) => {
-	const { name } = req.body;
+  const {name} = req.body;
 
-	if (!name) {
-		return res
-			.status(400)
-			.json({ success: false, msg: "please provide name value" });
-	}
+  if (!name) {
+    return res
+      .status(400)
+      .json({success: false, msg: "please provide name value"});
+  }
 
-	res.status(201).json({ success: true, person: name });
+  res.status(201).json({success: true, person: name});
 });
 
 app.post("/login", (req, res) => {
-	const { name } = req.body;
-	if (name) {
-		return res.status(200).send(`Welcome ${name}`);
-	}
-	res.status(401).send("Please Provide Credentials");
+  const {name} = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please Provide Credentials");
 });
 
 app.put("/api/people/:id", (req, res) => {
-	const { id } = req.params;
-	const { name } = req.body;
+  const {id} = req.params;
+  const {name} = req.body;
 
-	console.log(id, name);
-	res.send("Hello World");
+  console.log(id, name);
 });
 
 app.listen(3000, () => {
-	console.log("Server is listening on port 3000...");
+  console.log("Server is listening on port 3000...");
 });
---
+++ 2-express-tutorial/final/11-methods.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+let { people } = require("./data");
+
+// static assets
+app.use(express.static("./methods-public"));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+// parse json data
+app.use(express.json());
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  }
+
+  res.status(201).json({ success: true, person: name });
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please Provide Credentials");
+});
+
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+
+    return person;
+  });
+
+  res.status(200).json({ success: true, data: newPeople });
+});
+
+app.delete("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
+  }
+
+  const newPeople = people.filter((person) => person.id !== Number(id));
+
+  res.status(200).json({ success: true, data: newPeople });
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000...");
+});
